38a7d24d5db869fec3651d8458c022adcd0cb95c
Pedro H. Mota <pedro.rito@upe.br>
2024-06-12 14:56:31 -0300

    estilizacao e primeiros botoes da homepage

--- pokedexapp/lib/view/HomePage.dart
+++ pokedexapp/lib/view/HomePage.dart
@@ -4,45 +4,58 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  final Color textColor = Colors.white;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Color.fromRGBO(16, 16, 16, 1),
+        title: Center(
+          child: Text(
+            widget.title,
+            style: TextStyle(color: widget.textColor, fontSize: 24),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: widget.textColor),
+                    fixedSize: const Size(120, 80),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14)),
+                    )),
+                child: Text(
+                  'All Gens',
+                  style: TextStyle(color: widget.textColor, fontSize: 18),
+                )),
+            const SizedBox(
+              width: 14,
+            ),
+            OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: widget.textColor),
+                    fixedSize: const Size(120, 80),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14)),
+                    )),
+                child: Text(
+                  'Gen 1',
+                  style: TextStyle(color: widget.textColor, fontSize: 18),
+                )),
+          ],
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
--- pokedexapp/lib/view/MyApp.dart
+++ pokedexapp/lib/view/MyApp.dart
@@ -9,11 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(scaffoldBackgroundColor: Color.fromRGBO(33, 33, 33, 1)),
+      home: const MyHomePage(title: 'Pokedex'),
     );
   }
 }
